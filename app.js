c2ea98951dd40a9934e51e5aa88887aea0eadf4f
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2020-06-29 19:12:55 +0100

    set up server

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('hi world');
+import express from 'express';
+import session from 'express-session';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+const app = express();
+
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(bodyParser.urlencoded({ extened: true }));
+app.use(bodyParser.json());
+
+app.get('/', (request, response) => {
+  response.sendFiles(path.join(`${__dirname}/views/login.html`));
+});
