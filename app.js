21b145f0afe8124f8cc2d731aad3e9158a7801ef
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2020-06-30 16:07:03 +0100

    adding babel/node

--- app.js
+++ app.js
@@ -15,5 +15,10 @@ app.use(bodyParser.urlencoded({ extened: true }));
 app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
-  response.sendFiles(path.join(`${__dirname}/views/login.html`));
+  // response.sendFile(path.join(`${__dirname}/views/login.html`));
+  response.sendFile(path.join(`${__dirname}/views/login.html`));
+});
+
+app.listen(3000, () => {
+  console.log('app listening on port 3000');
 });
