eca8a15d39a8fc0340ca6094829866f91f153603
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2020-07-01 15:32:02 +0100

    adding login functionality

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+import 'dotenv/config';
 import express from 'express';
 import session from 'express-session';
 import bodyParser from 'body-parser';
 import path from 'path';
 
 const app = express();
+
+console.log(process.env.USERNAME, process.env.PASSWORD)
 
 // configure body-parser
 
@@ -20,6 +23,27 @@ app.use(session({
 app.get('/', (request, response) => {
   response.sendFile(path.join(`${__dirname}/views/login.html`));
 });
+
+app.post('/auth', (request, response) => {
+  console.log(request.session.resave, 'thi', request.body);
+
+  const { username, password } = request.body;
+  if(username && password) {
+    console.log('yes')
+    if(username === process.env.USERNAME && password === process.env.PASSWORD) {
+      request.session.loggedin = true;
+      request.session.username = username;
+      response.redirect('/home');
+    } else {
+      response.send('Incorrect Username and/or Password!');
+    }			
+    response.end();
+  }
+  else {
+    response.send('please enter a username and password');
+    response.end();
+  }
+})
 
 app.listen(3000, () => {
   console.log('app listening on port 3000');
