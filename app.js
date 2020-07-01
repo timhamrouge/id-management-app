a06da9f718714f9e8f8593d975b3fb12909efc68
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2020-07-01 10:10:40 +0100

    configuring bp

--- app.js
+++ app.js
@@ -5,17 +5,19 @@ import path from 'path';
 
 const app = express();
 
+// configure body-parser
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(session({
   secret: 'secret',
   resave: true,
   saveUninitialized: true,
 }));
 
-app.use(bodyParser.urlencoded({ extened: true }));
-app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
-  // response.sendFile(path.join(`${__dirname}/views/login.html`));
   response.sendFile(path.join(`${__dirname}/views/login.html`));
 });
 
