5c44ae47dd7f0360689ecfe2b35485cbdb181e54
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2020-07-02 18:49:15 +0100

    figuring out extensions

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ const app = express();
 
 // view engine setup
 app.set('view engine', 'hbs');
+app.engine('hbs', hbs());
 
-app.engine( 'hbs', hbs( {
-  extname: 'hbs',
-  defaultView: 'default',
-  layoutsDir: __dirname + '/views/layouts/',
-  partialsDir: __dirname + '/views/partials/'
-}));
+// app.engine( 'hbs', hbs( {
+//   extname: 'hbs',
+//   defaultView: 'default',
+//   layoutsDir: __dirname + '/views/layouts/',
+//   partialsDir: __dirname + '/views/partials/'
+// }));
 
 
 // configure body-parser
