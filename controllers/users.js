2a199c586050348b3c6288aaa71efe78d512739a
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2020-07-03 22:51:20 +0100

    refactoring routes

--- controllers/users.js
+++ controllers/users.js
@@ -4,41 +4,53 @@ import mongoose from 'mongoose';
 
 function createUser(req, res) {
     const { username, email, password } = req.body;
-    let user = new Users({username, email, password});
+    let user = new Users({ username, email, password });
 
-    mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
         console.log('connected')
         return user.save()
     }).then((x) => {
         console.log('created', x)
         mongoose.connection.close()
-    }).then(() => 
+    }).then(() =>
         console.log('closed and done'),
         res.send('ok')
     );
 }
 
-function findUser(req, res) {
+function authUser(request, response) {
     console.log('MADE IT SOOOOOON')
-    const { username, password } = req.body;
+    const { username, password } = request.body;
 
-    mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-        
-        return Users.findOne({username});
-    }).then((x) => {
-        if (x.password === password) {
-            res.render('home');
-        }
-        // mongoose.connection.close()
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-    // .then(() => 
-    //     console.log('closed and done'),
-    //     res.send('ok')
-    // );
+    if (username && password) {
 
+        mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+
+            return Users.findOne({ username });
+        }).then((x) => {
+            if (!x) {
+                response.send('looks like you made a mistake');
+                response.end();
+            }
+
+
+            if (x.password === password) {
+                request.session.loggedin = true;
+                request.session.username = username;
+            }
+            mongoose.connection.close();
+        }).then(() => {
+            console.log('closed and done'),
+            response.redirect('/home');
+        })
+    .catch ((err) => {
+            console.log(err);
+        })
+    }
+    else {
+        response.send('please enter a username and password');
+        response.end();
+    }
 }
 
-module.exports = { createUser, findUser }
+module.exports = { createUser, authUser }
--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {createUser, findUser} from './controllers/users'
+import { createUser, authUser } from './controllers/users'
 
 const api = express.Router();
 
@@ -7,7 +7,29 @@ const api = express.Router();
 
 //user - get one, check username/email, put to update, post to create, delete
 
+api.get('/', function (req, res, next) {
+    res.render('y', { layout: 'main', template: 'home-template' });
+});
+
+api.get('/login', (request, response) => {
+    response.render('login');
+});
+
+api.get('/home', (request, response) => {
+    if (request.session.loggedin) {
+        response.render('home');
+    } else {
+        response.send('please login to view this page');
+    }
+    response.end();
+})
+
 api.post("/registration", createUser);
-api.post("/auth", findUser)
+api.post("/auth", authUser)
+
+api.post('/logout', function (req, res) {
+    req.session.destroy()
+    res.render('login');
+})
 
 export default api;
