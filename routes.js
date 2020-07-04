6933f1f5d47da9c486e8883674d7eaac862f0ce6
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2020-07-04 22:07:30 +0100

    further work on routes

--- routes.js
+++ routes.js
@@ -4,47 +4,42 @@ import { destroySession } from './middleware';
 
 const api = express.Router();
 
-//endpoints we need
-
-
 // Server
 api.delete("/delete", deleteUser);
-
 api.get('/search', searchDetails);
-
 api.post("/auth", authUser);
 api.post("/create", createUser);
-
 api.put('/update', updateUser);
 
 // Client
-// api.get('/home')
-// api.get('/register')
-// api.get('/login')
+api.get('/', (req, res) => res.render('login'))
+api.get('/register', (req, res) => res.render('registration'))
+api.get('/home', (req, res) => res.render('home'))
+api.get('/login', (req, res) => res.render('login'));
 api.get('/logout', destroySession, (req,res) => res.redirect('login'));
 
 
 
 // view routes needs a registration, a login, a home/dashboard, a logout
-api.get('/home', (request, response) => {
-    if (request.session.loggedin) {
-        response.render('home');
-    } else {
-        response.send('please login to view this page');
-    }
-    response.end();
-})
+// api.get('/home', (request, response) => {
+//     if (request.session.loggedin) {
+//         response.render('home');
+//     } else {
+//         response.send('please login to view this page');
+//     }
+//     response.end();
+// })
 
-api.get('/register', function (request, response) {
-    response.render('registration', { layout: 'main', template: 'home-template' });
-})
+// api.get('/register', function (request, response) {
+//     response.render('registration', { layout: 'main', template: 'home-template' });
+// })
 
-api.get('/', function (req, res, next) {
-    res.render('y', { layout: 'main', template: 'home-template' });
-});
+// api.get('/', function (req, res, next) {
+//     res.render('y', { layout: 'main', template: 'home-template' });
+// });
 
-api.get('/login', (request, response) => {
-    response.render('login');
-});
+// api.get('/login', (request, response) => {
+//     response.render('login');
+// });
 
 export default api;
